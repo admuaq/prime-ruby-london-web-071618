eeba94bcec832e422dae89a2f9d5b88a44cae81a
Adham Muhammad <admuaq@gmail.com>
2018-07-09 16:26:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@
 # we should get the number 1. (As there will always be a remainder)
 
 def prime?(number) 
+  # Prevents numbers lower than 1 being passed as true 
  if number <= 1
   return false 
   end
