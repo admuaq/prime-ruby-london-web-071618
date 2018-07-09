99c0b11d6bf836cf8d6211c4543c8120bd22e0ec
Adham Muhammad <admuaq@gmail.com>
2018-07-09 16:04:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+ array = []
+ factor = 2 
+ 
+ while n > 1 
+    if (n%factor == 0)
+      array << factor
+      n /= factor
+    else 
+      factor += 1 
+    return array
+    end
+  end
+end
